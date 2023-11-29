c2c0f7938da2f2d6051a497fc3cb2e62be27c2fa
caio <caio.abreu@sou.inteli.edu.br>
2023-11-28 23:16:06 -0300

    refactor(bias-weight-calculation): :zap: Improved way of weight and bias calculation

--- p6/homework.py
+++ p6/homework.py
@@ -2,6 +2,8 @@
 
 # Importings
 import numpy as np
+import os
+from time import sleep
 
 # Logic Gates truth tables
 # AND Gate - 0 0 = 0, 0 1 = 0, 1 0 = 0, 1 1 = 1
@@ -9,45 +11,79 @@ import numpy as np
 # OR Gate - 0 0 = 0, 0 1 = 1, 1 0 = 1, 1 1 = 1
 # XOR Gate - 0 0 = 0, 0 1 = 1, 1 0 = 1, 1 1 = 0
 
-inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-
 class Perceptron:
-    def __init__(self):
-        pass
+    def __init__(self, num_inputs: list, epochs: int):
+        self.weights = np.random.rand(num_inputs)
+        self.bias = 0.0
+        self.epochs = epochs
 
-    def predict(self, inputs, threshold, weights):
-        total = sum(w * i for w, i in zip(weights, inputs))
-        return 1 if total > threshold else 0
+    def step_function(self, x):
+        return 1 if x >= 0 else 0
 
-    def train_perceptron(self, inputs, outputs, learning_rate, epochs):
-        weights = np.array([1.0] * len(inputs[0]))
-        threshold = 0.0
-        for epoch in range(epochs):
-            for input, output in zip(inputs, outputs):
-                prediction = self.predict(input, threshold, weights)
-                for i in range(len(weights)):
-                    weights[i] += learning_rate * (output - prediction) 
-                threshold += learning_rate * (output - prediction ) 
-            print("Threshold: ", threshold)
-            print("Epoch: ", epoch)
-            print("Weights: ", weights)
-        return weights, threshold
+    def train(self, inputs, outputs, learning_rate=0.1):
+        for self.epoch in range(self.epochs):
+            for i in range(len(inputs)):
+                input_data = inputs[i]
+                target_output = outputs[i]
 
-if __name__ == "__main__":
-    perceptron = Perceptron()
-    print("What kind of logic gate do you want to train? (AND, NAND, OR, XOR)")
-    logic_gate = input().upper()
-    if logic_gate == "AND":
-        outputs = np.array([0, 0, 0, 1])
-    elif logic_gate == "NAND":
-        outputs = np.array([1, 1, 1, 0])
-    elif logic_gate == "OR":
-        outputs = np.array([0, 1, 1, 1])
-    elif logic_gate == "XOR":
-        outputs = np.array([0, 1, 1, 0])
-    else:
-        print("Invalid input. Please choose from (AND, NAND, OR, XOR).")
+                weighted_sum = np.dot(input_data, self.weights) + self.bias
+                output = self.step_function(weighted_sum)
+
+                error = target_output - output
+
+                self.weights += learning_rate * error * input_data
+                self.bias += learning_rate * error
+
+    def test(self, inputs):
+        results = []
+        for input_data in inputs:
+            weighted_sum = np.dot(input_data, self.weights) + self.bias
+            output = self.step_function(weighted_sum)
+            results.append(output)
+        return results
+
+def main():
+    gate_types = ['AND', 'OR', 'NAND', 'XOR']
+
+    print("Escolha o tipo de porta lógica:")
+    for i, gate in enumerate(gate_types, start=1):
+        print(f"{i}. {gate}")
+
+    choice = int(input("Digite o número correspondente: "))
+
+    epochs = int(input("Digite o número de épocas: "))
+
+    if choice < 1 or choice > len(gate_types):
+        print("Escolha inválida. Saindo...")
         exit()
 
-    weights, threshold = perceptron.train_perceptron(inputs, outputs, 0.3, 10)
-    print(f'Prediction for the input {logic_gate}:', perceptron.predict([1, 1], threshold, weights))
+    selected_gate = gate_types[choice - 1]
+
+    if selected_gate == 'AND':
+        outputs = np.array([0, 0, 0, 1])
+    elif selected_gate == 'OR':
+        outputs = np.array([0, 1, 1, 1])
+    elif selected_gate == 'NAND':
+        outputs = np.array([1, 1, 1, 0])
+    elif selected_gate == 'XOR':
+        outputs = np.array([0, 1, 1, 0])
+
+    inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    
+    perceptron = Perceptron(num_inputs=len(inputs[0]), epochs=epochs)
+    perceptron.train(inputs, outputs)
+
+    results = perceptron.test(inputs)
+
+    print(f"\nPorta selecionada: {selected_gate}\n")
+    print(f'Predições: {results}')
+    print(f'Valores esperados: {outputs}')
+    print(f'Pesos: {perceptron.weights}')
+    print(f'Viés: {perceptron.bias}')
+
+    sleep(3)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+if __name__ == "__main__":
+    while True:
+        main()
